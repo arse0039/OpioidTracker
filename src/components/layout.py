0057c694462afc5ce57a24c35136dcf8b6c13450
Cristina Rosace <tina.rosace@gmail.com>
2022-09-11 16:47:00 -0400

    Front end

--- src/components/layout.py
+++ src/components/layout.py
@@ -1,50 +1,37 @@
 from threading import local
 from dash import Dash, html
-from . import state_dropdown, county_dropdown, general_resources, local_resources, death_graph, prescribing_graph
+import dash_bootstrap_components as dbc
+from . import state_dropdown, county_dropdown, general_resources, local_resources, death_graph, prescribing_graph, app_title, description
 
 
 def main_layout(app: Dash, data1, data2, data3) -> html.Div:
-    return html.Div(
-        className="main-div",
-        children=[
-            # header
-            html.Div(
-                children=[
-                    html.H1(app.title, className="title-header"),
-                    html.P(
-                        "Opioids are a class of drugs commonly used in the treatment of pain. According to the CDC, in 2019,\
-                20.4% of adults were loving with chronic pain and 7.4% experienced pain significant enough to \
-                    limit life or work activities. As a result, the prescribing of opioids in America has increased over the \
-                       past 23 years and with that has come an increased risk for addiction and opioid-related overdoses."),
-                    html.P("As prescribing rates and increased and new opioid-class drugs came on the market, the FDA began \
-                taking note of the increase in overdoses and in 2013 issued a letter describing the issue as the 'opioid epidemic'."),
-                    html.P("In 2020, 9.3 million people over the age of 12 misused prescription opioids within the prior year. \
-                During the same year, 75% of all overdose related deaths involved opioids."),
-                    html.Hr(), ],
+    drop_downs = [
+        dbc.Col(state_dropdown.render(app), width=4),
+        dbc.Col(county_dropdown.render(app), width=4)
+    ]
+    return html.Div(dbc.Container([
+            html.Br(),
+            dbc.Row(
+                   dbc.Col(
+                       app_title.render(app),
+                       width=12
+                   )
             ),
-            # dropdown
-            html.Div(
-                className="dropdown",
-                children=[
-                    state_dropdown.render(app),
-                    county_dropdown.render(app)]
-            ),
-            # graphs
-            html.Div(
-                className="graphs",
-                children=[
-                    prescribing_graph.render(app),
-                    death_graph.render(app),
-                ]
-            ),
-            # local info
-            local_resources.render(app),
-            # general info
-            general_resources.render(app),
-            # references?
-            html.Div(
-                'references?',
-                className="dropdown-div",
-            ),
-        ]
-    )
+
+            dbc.Row ([
+                    dbc.Col (
+                        prescribing_graph.render(app),
+                        width=6),
+        
+                    dbc.Col(
+                        death_graph.render(app),
+                        width=6)
+                    
+            ], className="g-0"),
+
+  
+
+        ]))
+        
+       
+
--- src/components/prescribing_graph.py
+++ src/components/prescribing_graph.py
@@ -1,16 +1,37 @@
 from dash import Dash, html, dcc
 import plotly.express as px
+import dash_bootstrap_components as dbc
+from . import state_dropdown
+
+
 
 fig = px.line(
     x=["2018", "2019", "2020"], y=[1, 3, 2],
-    title="Opioid Prescribing Data By State and County",
-    height=425,
-    labels={"x": "Year", "y": "Prescribing rate"},
+    labels={"x": "Year", "y": "Opioid Pescribing Rate"},
+    template='simple_white'
 )
+
+fig.update_traces(line_color='#304c94')
+fig.update_layout(title_text="Opioid Prescription by State and County", title_x=0.5, title_font_color="#712177")
 
 
 def render(app: Dash) -> html.Div:
     return html.Div(
-        className="graph",
-        children=dcc.Graph(figure=fig),
+        dbc.Card(
+            dbc.CardBody([
+                state_dropdown.render(app),
+                html.Br(),
+                dcc.Graph(
+                    figure=fig,
+                    id = 'death-graph',
+                    config={
+                        'displayModeBar': False
+                    }
+                )
+            ])
+
+        )
+
+        
+        
     )
