865eed3d312f1e7cb1ffb9484c48dd883778060e
Cristina Rosace <tina.rosace@gmail.com>
2022-09-11 22:06:02 -0400

    updated alternatives

--- src/components/layout.py
+++ src/components/layout.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 from . import state_dropdown, county_dropdown, general_resources, local_resources, death_graph, prescribing_graph, app_title, alternatives
 
 
-def main_layout(app: Dash, data1, data2, data3) -> html.Div:
+def main_layout(app: Dash) -> html.Div:
     drop_downs = [
         dbc.Col(state_dropdown.render(app), width=4),
         dbc.Col(county_dropdown.render(app), width=4)
